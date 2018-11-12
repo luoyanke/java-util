d492afe9cd0b38b08eb0ec208462f2ac9b339bb2
lyk <676292020@qq.com>
2018-11-12 15:03:19 +0800

    添加获取ip工具类

---
+++ src/main/java/com/sandboat/javautil/other/IpUtils.java
@@ -0,0 +1,30 @@
+package com.sandboat.javautil.other;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author lyk
+ * @Title: IpUtils
+ * @PACKAGE_NAME com.sandboat.javautil.other
+ * @Description: TODO
+ * @date 2018/11/1215:01
+ */
+public class IpUtils {
+
+    public static String getClientIp(HttpServletRequest request) {
+        String ip = request.getHeader("X-Real-IP");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Forwarded-For");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+}
